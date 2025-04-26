75577738b2fce8283ae4964376f282889554a50d
chris Taylor <ctay456a@gmail.com>
2025-04-25 23:35:17 -0400

    Create model.js

---
+++ model.js
@@ -0,0 +1,13 @@
+// models/User.js
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+// Define the user schema
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
