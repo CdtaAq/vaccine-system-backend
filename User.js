6bd72b36042105ba143f84541ef4c18a67af7e2e
chris Taylor <ctay456a@gmail.com>
2025-04-25 23:36:09 -0400

    Create User.js

---
+++ User.js
@@ -0,0 +1,22 @@
+// models/User.js
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+// Define the user schema
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ['Admin',
