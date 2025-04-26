daef61f4831792d3d9c06153efa2627538c5e836
chris Taylor <ctay456a@gmail.com>
2025-04-25 23:34:18 -0400

    Create setup.js

---
+++ setup.js
@@ -0,0 +1,8 @@
+// models/User.js
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required:
